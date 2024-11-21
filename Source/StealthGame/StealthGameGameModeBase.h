4e6bdd8335e08cf5cf43a60e346e9f89f4c62253
GeorgGrech <fgegr001@edu.xamk.fi>
2024-11-21 16:59:42 +0100

    init commit

---
+++ Source/StealthGame/StealthGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "StealthGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STEALTHGAME_API AStealthGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
